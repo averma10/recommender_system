6d655e19856c772c1043b4ce009056a60f105c52
averma10 <anuj.vermacom@gmail.com>
2019-05-23 14:22:04 -0500

    added modified code

--- code/clean_code/Word2Vec.py
+++ code/clean_code/Word2Vec.py
@@ -1,62 +1,180 @@
+import itertools
+from collections import defaultdict
+import os
 
 import pandas as pd
+import numpy as np
 
 import gensim
 from gensim.parsing.preprocessing import remove_stopwords
 from gensim.models.word2vec import LineSentence
 from nltk.corpus import stopwords
 
-import itertools
-from collections import defaultdict
-import os
-
-
 # Extract all the words from user_reviews summary and store as a list of words
 #######################################
-
-def build_data(dfname):
+def extract_data(extract_from,features_to_extract = []):
     '''
-    Extract the user reviews summary data from dataset and store user reviews as a list of lists.
+    Extract the listed features from the file, save the extrated features and return the dataframe.
+    ##old-Extract the user reviews summary data from dataset and store user reviews as a list of lists.
     Args:
-        dfname: name of dataset whose user reviews summary needs to be extracted.
-                Valid values are ['movies','games','music','tv']
-    Returns:
-        A list of all the sentences/reviews found in user reviews summary.
-    '''
-    filename = os.path.join('data','user_reviews_by_{}.csv').format(dfname)
-    df = pd.read_csv(filename,delimiter = '|',engine = 'python')
+        extract_from:[str] filename from which information will be extracted. 
+                Assumes that the file is delimited by pipe '|'. Does not work for anyother delimiter type.
+        save_to:[str] filename to save the extracted information. It will be stored as a csv format.
+        features_to_extract:[list] list of features to extract from the extract_from filename. 
+                If list is empty then all features of the dataset are extracted.
+        ##old - dfname: name of dataset whose user reviews summary needs to be extracted.
+        ##        Valid values are ['movies','games','music','tv']
     
-    arr = []
-    filename = os.path.join('data','summary_{}.txt').format(dfname)
+    Returns:
+        [pandas.DataFrame] A dataframe with extracted features. 
+        ##old -A list of all the sentences/reviews found in user reviews summary.
+    '''
+    
+    filename = os.path.join('data',extract_from)
+    if len(features_to_extract):
+        #try:
+        df = pd.read_table(filename,delimiter = '|',engine = 'python',usecols=features_to_extract)
+        #except FileNotFoundError:
+            
+            
+        
+    else:       
+        df = pd.read_table(filename,delimiter = '|',engine = 'python')
+    
+    return df
+    
+
+def process_reviews(series):
+    '''
+    Proces reviews/summary to tokenize the posts and remove the english stopwords.
+    
+    '''
+    
+    
+    series = series.apply(gensim.utils.simple_preprocess)
+    
+    eng_stopwords = stopwords.words('english')
+    remove_stopwords = lambda line: [word for word in line if word not in eng_stopwords]
+    
+    return series.apply(remove_stopwords)
+
+
+def process_features(df,list_of_prefixes):
+    '''
+    insert the prefixes in the dataset for user_id,item_id etc.
+    '''
+    sep = '_'
+    columns = list(df.columns.values)
+    columns.remove('summary')
+    #prefix = ['u_','i_']
+    
+    #check if the column and prefixes lists are of same size, raise IndexError otherwise
+    if len(columns) != len(list_of_prefixes):
+        raise Exception('Number of features does not match the prefixes provided. \
+                            features={} and list_of_prefixes={}'.format(len(columns),len(list_of_prefixes)))
+    
+    idx=0
+    while(idx<len(columns)):
+        df.iloc[:,idx:idx+1] = list_of_prefixes[idx] + sep + df.iloc[:,idx:idx+1]
+        idx+=1
+    
+    df.summary = process_reviews(df.summary)
+    
+    return df  
+    
+def preprocess_data(filename,list_of_features=[],list_of_prefixes=[]):
+    '''
+    Main function, calls these functions 1. extract_data, 2. process_features
+    Args: 
+        filename:[str] filename of stored dataset
+        list_of_features:[list] list of features to extract from the dataset
+        list_of_prefixes:[list] list of prefixes to include in features other than reviews.
+                Assumptions:
+                    1. prefixes are provided in same order as features
+                        e.g. for features=['user_id','item_id'], prefixes=['u','i']
+                    2. The default seperator '_' is used
+                    3. length of prefixes list is always equal to len(list_of_features)-1
+    Returns:
+        [pandas.DataFrame] feature columns with prefixed characters and summary column tokenized and ready for injection. 
+                    
+        
+    '''
+    if len(list_of_prefixes)!= len(list_of_features)-1:
+        raise ValueError('The precondition: len(prefixes)== len(feature_lst)-1 not met. \nPlease check the arguments provided.')
+    
+    #1. extract features from dataset
+    df = extract_data(filename,list_of_features)
+    
+    #clean the data to remove all null values
+    df.dropna(axis=0,how='any',inplace=True)
+    
+    #2. process the information before insertion
+    
+    df = process_features(df,list_of_prefixes)
+    
+    return df
+
+
+def inject_information_in_reviews(df,list_of_features,save_to,inject_window_size = 15):
+    '''
+    The function injects the information from listed features into user reviews/user posts after specified windowsize
+    This function should be called after preprocessing all the data especially user posts summary.
+    '''
+    list_of_features.remove('summary')
+    #iterate over rows of dataset
+    for index,row in df.iterrows():
+        #iterate over list of features and store the values
+        #store values to inject in user-reviews
+        val_lst = [row[val] for val in list_of_features]
+        #u = row['user_id']
+        #i = row['token_name']
+        #injected summary
+        new_summary = row['summary']
+        #print(lst)
+        idx = 0
+        while(idx <=len(new_summary)):
+            #[lst.insert(idx,a) for a in [i,u]]
+            [new_summary.insert(idx,val) for val in val_lst]
+            idx +=inject_window_size+len(val_lst)
+        
+        #replace summary with injeced summary
+        row['summary'] = ' '.join(new_summary)
     
     arr = np.array(df.summary)
     
-    np.savetxt(filename,arr,fmt='%-5s',encoding='utf-8')
-    
-    print('Summary Data successfully saved in file:"{}"'.format(filename))
-    
-    
-def exclude_stopwords(list_of_sentences):
-    '''
-    Function to remove stopwords from sentences. This function uses nltk library and removes only english stopwords.
-    Use this function after splitting the sentences into word list
-    Requirements:
-        This function requires the stopwords data downloaded on local machine. Before calling this function ensure to download the stopwords using nltk.download('stopwords')
-    Arguments:
-        list_of_sentences:[list of lists] list of sentences where each sentence is a list of words.
-    Returns:
-        [list of lists] After removing stopwords: list of sentences where each sentence is a list of words.
-    '''
+    #return arr
+    #print(arr)
+    filename = os.path.join('data',save_to)
     try:
-        stopwords_set = set(stopwords.words('english'))
-    except LookupError as e:
-        raise Exception('Not able to find stopwords. Check if these are downloaded on local machine.\nError Message:\n',e)
+        np.savetxt(filename,arr,fmt='%-5s',encoding='utf-8')
+        print('File successfully saved at location: {}'.format(filename))
+    except Exception as e:
+        print('Error in saving injected information file.\n',e)    
     
-    return [[word for word in sentence if word not in stopwords_set] \
-             for sentence in list_of_sentences]
 
+def create_sentences(extract_from,list_of_features,list_of_prefixes,save_to,inject_window_size=15):
+    '''
+    This function readies the data by first reading it from the disk location and then processing the data. 
+    Two processings are applied on the data:
+        1. gensim.utils.simple_preprocess() removes any digits, special characters, punctuation marks from the sentences and the output is generated in list_of_list format.
+        2. nltk.corpus.stopwords() is used to remove stopwords from the list_of_lists formatted output from step 1.
+    Arguments:
+        dfname: name of dataset whose user reviews summary needs to be extracted.
+                Valid values are ['movies','games','music','tv','all']
+    Returns:
+        Nothing.
+        Prints if the processed and injected file is saved in memory or not.
+    '''
+    #preprocess data
+    df = preprocess_data(extract_from,list_of_features,list_of_prefixes)
+    
+    #inject informaion in reviews
+    inject_information_in_reviews(df,list_of_features,save_to,inject_window_size)
+    #read the data from disk location
+    #data = read_data(dfname)
+    #return exclude_stopwords(list(data))
 
-def read_data(dfname):
+def read_sentences(filename):
     '''
     Read the data from the disk location. 
     Using the simple_preprocess() method of gensim, 
@@ -75,38 +193,33 @@ def read_data(dfname):
     '''
     #load data from file
     loc = 'data'
-    file = 'summary_{}.txt'.format(dfname)
-    file_path = os.path.join(loc,file)
+    #file = 'summary_{}.txt'.format(dfname)
+    file_path = os.path.join(loc,filename)
     
     if os.path.isfile(file_path):
         with open(file_path,'rb') as f:
             for line in f:
-                yield gensim.utils.simple_preprocess(line)
+                #print(line)
+                yield line.split()
     else:
         raise FileNotFoundError('There is no file "{}" at location "{}"'.format(file,os.path.abspath(loc)))
-
-        
-        
-        
-def ready_data(dfname = 'all'):
-    '''
-    This function readies the data by first reading it from the disk location and then processing the data. 
-    Two processings are applied on the data:
-        1. gensim.utils.simple_preprocess() removes any digits, special characters, punctuation marks from the sentences and the output is generated in list_of_list format.
-        2. nltk.corpus.stopwords() is used to remove stopwords from the list_of_lists formatted output from step 1.
-    Arguments:
-        dfname: name of dataset whose user reviews summary needs to be extracted.
-                Valid values are ['movies','games','music','tv','all']
-    Returns:
-        [[lists]] Processed data ready for use in Word2Vec model
-    '''
-    #read the data from disk location
-    data = read_data(dfname)
-    return exclude_stopwords(list(data))
 
 
 #Word2Vec model Accuracy
 #################################
+def word_keep_rule(word, word_count, min_count):
+    """
+    This function is used only by gensim's word2vec. A rule to decide whether to keep a word or discard it in gensim.word2vec model.
+    This rule is used to keep injected users and items in the word2vec vocabulary. We want all of these so that we can recommend things to any user or item.
+    Gensim's word2vec will call this rule on every vocab word to decide if its kept. 
+    If the word is prefixed by 'u:' or 'm:', we keep it. Otherwise, we choose the default behavior (which is: discard it if word_count < min_count).
+    For a reference on this default behavior, see
+    https://github.com/piskvorky/gensim/blob/develop/gensim/models/word2vec.py#L404
+    """
+    if word[:1] == 'u' or word[:1] == 'i':
+        return gensim.utils.RULE_KEEP
+    return gensim.utils.RULE_DEFAULT
+
 
 def w2v_model_accuracy(model):
     questions = 'questions-words.txt'
